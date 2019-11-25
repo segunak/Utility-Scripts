d974dce799bd816a8825cbec669e1da43353bd87
Segun <akintundejr4@gmail.com>
2019-11-25 11:12:05 -0600

    New scripts

--- psxconnect.cmd
+++ psxconnect.cmd
@@ -1,7 +1,7 @@
 @Echo Off
 setLocal enableDelayedExpansion
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: pse-connect.cmd 
+:: psxconnect.cmd 
 ::
 :: Opens a CMD session on a specified PC via PsExec, given it's host name. This
 :: script assumes you have PSTools installed on your PATH.  
