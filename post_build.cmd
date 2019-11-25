0520a86581f9afd6dc11f3630120e4a7f6a446ad
Segun <akintundejr4@gmail.com>
2019-11-25 11:48:18 -0600

    Updates

--- post_build.cmd
+++ post_build.cmd
@@ -35,6 +35,12 @@ if $(ConfigurationName) == Release (
 xcopy "$(ProjectDir)bin\release\MyCoolFileThatIWantCopied.exe" "$(SolutionDir)FolderThatIWantFileCopiedTo" /q /y
 )
 
+:: Another example of copying file from a folder to another based on type of config.
+if $(ConfigurationName) == Release (
+for /R %%f in (*) do (if not "%%~xf"==".exe" del "%%~f")
+xcopy "$(ProjectDir)bin\Release\GrabObject.exe" "C:\RSS-Local\Misc\Utilities" /q /y
+)
+
 :: Copy a file from one one location to another based on relative path. The ../ is taking
 :: you up from the directory of the solution. 
 if $(ConfigurationName) == Release (
